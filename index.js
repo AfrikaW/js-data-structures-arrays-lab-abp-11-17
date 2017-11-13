c77cec50a7d598c2149dba75b53f6ea49bf1a5e8
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-13 07:05:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution here!
+// array called drivers == ["Milo", "Otis", "Garfield"]'
+const drivers=["Milo", "Otis", "Garfield"]
+
+
+  function destructivelyAppendDriver(name) {
+    drivers.push(name)
+  };
+
+  function destructivelyPrependDriver(name){
+    drivers.unshift(name)
+  };
+
+  function destructivelyRemoveLastDriver(name) {
+    drivers.pop(name)
+  };
+
+  function destructivelyRemoveFirstDriver(name) {
+    drivers.shift(name)
+  };
+
+  function appendDriver(name) {
+    const drivers2 = [`${name}`, ... drivers]
+  };
