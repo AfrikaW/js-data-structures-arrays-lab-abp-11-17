a866565e99e15652de2dfd6bb6e04ccaf775256e
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-13 07:19:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ const drivers=["Milo", "Otis", "Garfield"]
 
   function appendDriver(name) {
     const copyDrivers = drivers.slice();
-      return Drivers.push(name);
+      return drivers.push(name);
   };
